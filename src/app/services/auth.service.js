44d3699de84a1f39c5888b7d441781d9663217f0
AzrizHaziq <haziq@nazrol.tech>
2017-10-20 17:59:43 +0800

    update AuthService

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -2,4 +2,21 @@ AuthService.$inject = []
 
 export default function AuthService() {
 
+    const state = {
+        is_login : false
+    }
+
+    const isLogin = () => state.is_login
+
+    const userAuthenticate = () => state.is_login = true
+
+    const userUnAuthenticate = () => state.is_login = false
+
+    return {
+        shareableState : state,
+        isLogin,
+        userAuthenticate,
+        userUnAuthenticate
+    }
+
 }
